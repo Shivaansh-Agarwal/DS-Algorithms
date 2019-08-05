466fa968ead00c518e0d861e2af2dcc5d1258ea2
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2019-08-05 08:37:27 +0530

    Given an array and a sum x find if there is subarray where sum is equal to x

---
+++ Hashing/05.cpp
@@ -0,0 +1,22 @@
+/* 
+    PROBLEM STATEMENT
+    Given an array and a sum x find if there is subarray where sum is aqual to x.
+
+    I/P: arr[] = {5,3,8,-2,8,10}
+    O/P: true
+*/
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int main(){
+    int n,x;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<"Enter the value of x: ";
+    cin>>x;
+    
+}
