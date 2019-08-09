245ecf7d47e87dd23606f6fcc31df1fe4b96824b
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2019-08-09 08:28:33 +0530

    Changes to 10.cpp

--- Hashing/10.cpp
+++ Hashing/10.cpp
@@ -11,3 +11,21 @@
     I/P: arr[] = {1,1,1,1,0,0,1,1,1,1,0,0}
     O/P: 8
 */
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int findLength(int *arr,int n){
+    
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    cout<<"Enter the elements: ";
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int ans = findLength(arr,n);
+    cout<<"Length of largest subarray having equal number of 0's and 1's is: "<<ans;
+    return 0;
+}
