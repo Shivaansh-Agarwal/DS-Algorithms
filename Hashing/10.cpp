1ef10131d9ac05c43499a14ec726c618f9d9a0a5
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2019-08-09 08:25:39 +0530

    Given a binary array find the length of the largets subarray having equal number of 0's and 1's

---
+++ Hashing/10.cpp
@@ -0,0 +1,13 @@
+/* 
+    PROBLEM STATEMENT:
+    Given a binary array find length of the largest subarray having equal number of 0's and 1's
+
+    I/P: arr[] = {0,1,1,0,1,1,0}
+    O/P: 4
+
+    I/P: arr[] = {1,1,1,1}
+    O/P: 0
+
+    I/P: arr[] = {1,1,1,1,0,0,1,1,1,1,0,0}
+    O/P: 8
+*/
